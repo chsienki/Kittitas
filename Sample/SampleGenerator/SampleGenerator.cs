ba3a1cf6c5b12331333f4c3b25dc44ff2a7d3de7
Chris Sienkiewicz <chsienki@microsoft.com>
2020-09-02 14:32:30 -0700

    Add sample application that shows how to use kittitas

---
+++ Sample/SampleGenerator/SampleGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace SampleGenerator
+{
+    [Generator]
+    public class SampleGenerator : ISourceGenerator
+    {
+        public void Execute(SourceGeneratorContext context)
+        {
+            Debugger.Break();
+            context.AddSource("example", SourceText.From("public class ExampleClass{}", Encoding.UTF8));
+        }
+
+        public void Initialize(InitializationContext context)
+        {
+        }
+    }
+}
---
+++ Sample/UsingGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppUsingGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This application uses the SampleGenerator project. 
+            // We'll run the build of this project through roslyn when we launch kittitas
+        }
+    }
+}
